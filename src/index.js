fd3725bd11887f54c0bab6652912bda4d534cb6d
GabrielSantos <ghs.developer@outlook.com>
2019-11-24 10:59:25 -0300

    Creation of index
    Import react and render new tag in html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//selecionando a div com id == root dentro do body
+const elemento = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//chamando a função para renderizar o conteúdo na página
+ReactDOM.render(<h1>Olá React</h1>, elemento); // Texto || tag(precisa do import do react) + elemento
+
+
+
