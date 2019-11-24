f8d541a47292f0f0f1cda78e50448ce47337b8fc
GabrielSantos <ghs.developer@outlook.com>
2019-11-24 11:12:53 -0300

    Using the tag in a var
    Tag can be used even outside current file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom';
 //selecionando a div com id == root dentro do body
 const elemento = document.getElementById('root');
 
+const jsx = <h1>Olá React</h1>;
+
 //chamando a função para renderizar o conteúdo na página
-ReactDOM.render(<h1>Olá React</h1>, elemento); // Texto || tag(precisa do import do react) + elemento
+ReactDOM.render(jsx, elemento); // Texto || tag(precisa do import do react) + elemento
 
 
 
