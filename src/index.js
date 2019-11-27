7b7703ee92e2029b6f8d040d5fb645d251d0960e
Gabriel Santos <ghs.developer@outlook.com>
2019-11-27 09:00:47 -0300

    Import e export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import PrimeiroComponente from './componentes/PrimeiroComponente';
 // import { CompA, CompB as B } from './componentes/DoisComponentes';
-import MultiElementos from './componentes/MultiElementos';
+import CompA, { CompB as B } from './componentes/DoisComponentes';
+// import MultiElementos from './componentes/MultiElementos';
 
 //selecionando a div com id == root dentro do body
 const elemento = document.getElementById('root');
@@ -32,9 +33,17 @@ const elemento = document.getElementById('root');
 //   , elemento
 // );
 
+// ReactDOM.render(
+//   <div>
+//     <MultiElementos />
+//   </div>
+//   , elemento
+// );
+
 ReactDOM.render(
   <div>
-    <MultiElementos />
+    <CompA valor="Olá eu sou o A" />
+    <B valor="Olá eu sou o B" /> {/* Renomeando um componente */}
   </div>
   , elemento
 );
