2a72490a657ba2261353c8978ffc93e24be92dc2
Gabriel Santos <ghs.developer@outlook.com>
2019-11-27 08:21:06 -0300

    Componentes com propriedades

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -6,4 +6,13 @@ import React from 'react';
 
 // export default primeiro;
 
-export default () => <h1>Primeiro Componente!</h1>; // como função anônima + arrow function
+// export default () => <h1>Primeiro Componente!</h1>; // como função anônima + arrow function
+
+export default props =>
+  <div>
+    <h1>{props.valor}</h1> {/* Forma de passar um valor pra ser renderizado */}
+    <h1>{props.AbCd}</h1>
+    <h1>{props.calc}</h1>
+    <h1>{props.math}</h1>
+    <h1>{props.txt}</h1>
+  </div>
--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ const elemento = document.getElementById('root');
 //chamando a função para renderizar o conteúdo na página
 //ReactDOM.render(jsx, elemento); // Texto || tag(precisa do import do react) + elemento
 
+let isLegal = true;
+// let isLegal = false;
+
 ReactDOM.render(
   <div>
-    <PrimeiroComponente /> {/* Forma de chamar o componente (e de comentar) */}
+    <PrimeiroComponente valor="Bom dia!" /> {/* Forma de chamar o componente (e de comentar) */}
+    <PrimeiroComponente AbCd={123} /> {/* Outra forma de enviaro o valor a um componente */}
+    <PrimeiroComponente calc={2**8} /> {/* 2 elevado a 8 */}
+    <PrimeiroComponente math={Math.pow(2, 8)} /> {/* 2 elevado a 8 em função */}
+    <PrimeiroComponente txt={isLegal ? 'Codar é legal' : 'Codar não é legal'} />
   </div>
   , elemento);
